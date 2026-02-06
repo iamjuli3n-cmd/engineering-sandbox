dc5ba0582af3df5596c946a2f56a9dd8e1c1de7b
iamjuli3n-cmd <julienblondiaux78400@gmail.com>
2026-02-06 17:02:49 +0100

    cleaning main code

--- todo.py
+++ todo.py
@@ -1,4 +1,5 @@
 from datetime import datetime
+from functions import add_task, show_tasks, modify_task
 
 today = datetime.today()
 
@@ -34,75 +35,13 @@ while running:
     choix = input("Add / Show / Modify / Quit")
 
     if choix == "Add":
-        titre = input("quel est cette tache ? ")
-        reponse = input("Terminé ? (y/n) : ")
-        conv = reponse == "y"
-
-        date_creation = input("Date de création (YYYY-MM-DD) : ")
-        deadline = input("Deadline (YYYY-MM-DD) : ")
-        rappel = int(input("Rappel en minutes : "))
-
-        task = {
-            "title": titre,
-            "done": conv,
-            "date_creation": date_creation,
-            "deadline": deadline,
-            "rappel": rappel,
-        }
-        tasks.append(task)
+        add_task(tasks)
 
     elif choix == "Show":
-        i = 1
-        while i <= len(tasks):
-            task = tasks[i - 1]
-            status = "✔" if task["done"] else "✘"
-            print(f"{i}. {task['title']} [{status}]")
-            print(f"   Créée le : {task['date_creation']}")
-            print(f"   Deadline : {task['deadline']}")
-            print(f"   Rappel : {task['rappel']} minutes")
-            i += 1
+        show_tasks(tasks)
 
     elif choix == "Modify":
-        i = 1
-        while i <= len(tasks):
-            print(f"{i}. {tasks[i - 1]['title']}")
-            i += 1
-
-        index = int(input("Numéro de la task à modifier : ")) - 1
-
-        if 0 <= index < len(tasks):
-            task = tasks[index]
-
-            print("\nQue veux-tu modifier ?")
-            print("1. Titre")
-            print("2. Terminé")
-            print("3. Date de création")
-            print("4. Deadline")
-            print("5. Rappel")
-
-            choix_modif = input("Choix : ")
-
-            if choix_modif == "1":
-                task["title"] = input("Nouveau titre : ")
-
-            elif choix_modif == "2":
-                rep = input("Terminé ? (y/n) : ")
-                task["done"] = rep == "y"
-
-            elif choix_modif == "3":
-                task["date_creation"] = input("Nouvelle date de création : ")
-
-            elif choix_modif == "4":
-                task["deadline"] = input("Nouvelle deadline : ")
-
-            elif choix_modif == "5":
-                task["rappel"] = int(input("Nouveau rappel (minutes) : "))
-
-            else:
-                print("Choix invalide.")
-
-        else:
-            print("Task inexistante.")
+        modify_task(tasks)
 
     elif choix == "Quit":
         running = False
